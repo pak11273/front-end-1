27459c490a502c055e611ac6a83103d0d2d9c548
nickcabuno <nickcabuno17@gmail.com>
2021-03-01 20:52:50 -0500

    newnew

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React, {useEffect} from 'react'
+import React from "react";
+import {Route} from 'react-router-dom';
+import Home from "./components/Home";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 import './App.css';
-import LoginForm from './components/LoginForm'
-
-import axios from 'axios'
+import DisplayPage from './components/DisplayPage'
+import EditRecipe from './components/EditRecipe'
 
 function App() {
 
-
-
-
-
-
-
   return (
     <div className="App">
-
-
-
-
-
       
-        <LoginForm />
+      <Route exact path="/" component={Home}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/signup" component={SignUp}/>
+      <Route path="/edit" component={EditRecipe}/>
+      <Route path="/display" component={DisplayPage}/>
 
     </div>
   );
 }
+
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,97 @@
-import React from 'react'
 import LoginForm from './LoginForm'
-import RegisterForm from './RegisterForm'
+import React, {useState, useEffect} from "react";
+import loginSchema from './loginSchema'
+import * as yup from 'yup'
+import {Link} from "react-router-dom";
+import axios from 'axios'
 
+const initialFormValues = {
+    username: '',
+    // firstname: '',
+    // lastname: '',
+    // email: '',
+    password: '',
+    // title: '',
+    // category: '',
+    // source: '',
+    // ingredients: '',
+    // instructions: '',
+  }
+  const initialFormErrors = {
+    username: '',
+    // firstname: '',
+    // lastname: '',
+    // email: '',
+    password: '',
+    // title: '',
+    // category: '',
+    // source: '',
+    // ingredients: '',
+    // instructions: '',
+  }
 
+  const initialDisabled = true
 
-function Login() {
-  return (
-    <div>
-      <h2>Loginingggggggg</h2>
+export default function Login() {
 
-      {/* 🤞🐱‍🐉🎂 */}
-      <LoginForm />
-      <RegisterForm />
+    const [formValues, setFormValues] = useState(initialFormValues)
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
+    const [disabled, setDisabled] = useState(initialDisabled)
+
+    const inputChange = (name, value) => {
+        yup.reach(loginSchema, name)
+          .validate(value)
+          .then(() => {
+            setFormErrors({...formErrors, [name]: ''})
+          })
+          .catch(err => {
+            setFormErrors({...formErrors, [name]: err.errors[0]})
+          })
+        setFormValues({
+          ...formValues,
+          [name]: value 
+        })
+    }
+
+    const formSubmit = () => {
+        const loginSubmit = {
+            username: formValues.username.trim(),
+            //firstname: formValues.firstname.trim(),
+            //lastname: formValues.lastname.trim(),
+            //email: http://formValues.email.trim(),
+            password: formValues.firstname.trim(),
+            // title: formValues.title.trim(),
+            // category: formValues.category.trim(),
+            // source: formValues.source.trim(),
+            // ingredients: formValues.ingredients.trim(),
+            // instructions: formValues.instructions.trim(),
+        }
+        axios.post('', loginSubmit).then(res => {
+          console.log(res)
+        }).catch(e => console.log(e))
+      };
+
+      useEffect(() => {
+      loginSchema.isValid(formValues).then(valid => 
+        setDisabled(!valid))
+      }, [formValues])
+
+return(
+    <div className='login'>
+        <h1>.</h1>
+
+        <LoginForm
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+        />
+
+        <div>
+            <Link to="/">no log in thx</Link>
+        </div>
 
     </div>
-  )
+    )
 }
-
-export default Login
