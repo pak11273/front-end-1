908cbf647d161aa047699ce1d61ebbc19114d19d
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-03-01 21:11:38 -0500

    mini testing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-
 import React from "react";
 import {Route} from 'react-router-dom';
+
 import Home from "./components/Home";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-
 import './App.css';
-import LoginForm from './components/LoginForm'
 
-import axios from 'axios'
+import Header from './components/Header'
+
 
 function App() {
 
 
-
-
-
-
-
-
-
   return (
     <div className="App">
-
-
-
-
-
       
+      <Header />
+      
+
+
+
+
 
       <Route exact path="/" component={Home}/>
       <Route path="/login" component={Login}/>
