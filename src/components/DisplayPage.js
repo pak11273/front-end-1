e1049fc8b20f3a34a85a0611c1944e3379046413
nickcabuno <nickcabuno17@gmail.com>
2021-03-01 20:56:38 -0500

    update2

--- src/components/DisplayPage.js
+++ src/components/DisplayPage.js
@@ -1,35 +1,35 @@
-import React from 'react'
+// import React from 'react'
 
-function DisplayPage() {
+// function DisplayPage() {
 
-  const recipe = {
-    title: 'Chocolate Cake',
-    category: 'cake, dessert,chocolate',
-    source: 'online',
-    ingredients: 'chocolate, flour, eggs, sugar, milk',
-    instructions: 'mix flour, eggs, sugar, and milk, melt chocolate and mix in, then bake.',
-    user_id: 1
-  };
+//   const recipe = {
+//     title: 'Chocolate Cake',
+//     category: 'cake, dessert,chocolate',
+//     source: 'online',
+//     ingredients: 'chocolate, flour, eggs, sugar, milk',
+//     instructions: 'mix flour, eggs, sugar, and milk, melt chocolate and mix in, then bake.',
+//     user_id: 1
+//   };
 
-  const ingredientsArray = recipe.ingredients.split(',');
+//   const ingredientsArray = recipe.ingredients.split(',');
 
-  return (
-    <div>
-      <h1>{recipe.title}</h1>
-      {/* the links on the following line should be replaced with React Links once routing is set up */}
-      <a href='/'>Edit Recipe</a> <a href='/'>Delete Recipe</a>
-      <p>categories: <em>{recipe.category}</em></p>
-      <p>source: {recipe.source}</p>
-      <h2>Ingredients</h2>
-      <ul>
-        {ingredientsArray.map((ingredient, index) => {
-          return <li key={index}>{ingredient}</li>;
-        })}
-      </ul>
-      <h2>Directions</h2>
-      <p>{recipe.instructions}</p>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h1>{recipe.title}</h1>
+//       {/* the links on the following line should be replaced with React Links once routing is set up */}
+//       <a href='/'>Edit Recipe</a> <a href='/'>Delete Recipe</a>
+//       <p>categories: <em>{recipe.category}</em></p>
+//       <p>source: {recipe.source}</p>
+//       <h2>Ingredients</h2>
+//       <ul>
+//         {ingredientsArray.map((ingredient, index) => {
+//           return <li key={index}>{ingredient}</li>;
+//         })}
+//       </ul>
+//       <h2>Directions</h2>
+//       <p>{recipe.instructions}</p>
+//     </div>
+//   )
+// }
 
-export default DisplayPage
+// export default DisplayPage
