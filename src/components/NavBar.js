e6635707be3478b7123a47ce0fccfe53a0bd4838
joe-park <parkjs3@yahoo.com>
2021-03-02 15:32:20 -0800

    basic dashboard

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,41 @@
+import styled from "styled-components";
+
 export const NavBar = () => {
-    return (
-        <nav>
-            <div className="logo">
-              <p>Secret Food Recipes!</p>
-            </div>
-            <ul className='nav-links'>
-                <li>
-                  <a href='/'>Home</a>
-                </li>
-                <li>
-                  <a href='/login'>Login</a>
-                </li>
-                <li>
-                  <a href='/signup'>Sign Up</a>
-                </li>
-             </ul>
-        </nav>
-    )
-}
+  return (
+    <NavStyle>
+      <div>
+        <TitleStyle>Secret Food Recipes!</TitleStyle>
+      </div>
+      <LinksStyle>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+        <li>
+          <a href="/signup">Sign Up</a>
+        </li>
+      </LinksStyle>
+    </NavStyle>
+  );
+};
+
+const NavStyle = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  min-height: 4vh;
+  background-color: wheat;
+`;
+const TitleStyle = styled.p`
+  color: black;
+  text-transform: uppercase;
+  letter-spacing: 1%;
+  font-size: 0.8em;
+  font-family: Georgia;
+`;
+
+const LinksStyle = styled.ul`
+  display: flex;
+`;
