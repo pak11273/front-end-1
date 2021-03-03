170d937d904e3e0231a87bdb67c383772d5c44c0
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-03-03 15:17:21 -0700

    styling started

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,48 @@
 import React from "react";
+import styled from 'styled-components'
+
+//styling for loginform 
+const StyledLoginForm = styled.form`
+    color: ${pr => pr.theme.fontColor};
+    font-size: ${pr => pr.theme.fontSize.medium};
+    font-weight: bold;
+    background-color: ${pr => pr.theme.tertiaryColor};
+    display: ${pr => pr.theme.display};
+    justify-content: ${pr => pr.theme.justifyContent};
+    flex-wrap: ${pr => pr.theme.flexWrap};
+    border-radius: ${pr => pr.theme.borderRadius};
+
+    h2{
+    font-size: ${pr => pr.theme.fontSize.large};
+    white-space: ${pr => pr.theme.whiteSpace};
+    color: ${pr => pr.theme.fontColor};
+    background-color: ${pr => pr.theme.secondaryColor};
+    padding: ${pr => pr.theme.padding.titlePadding};
+    }
+  input{
+    height: ${pr => pr.theme.input.height};
+    width: ${pr => pr.theme.input.width};
+    border-radius: ${pr => pr.theme.input.borderRadius};
+    }
+  button{
+    color: ${pr => pr.theme.fontColor};
+    padding: ${pr => pr.theme.padding.buttonPadding};
+    font-size: ${pr => pr.theme.fontSize.button};
+    margin: ${pr => pr.theme.margin.button};
+    white-space: ${pr => pr.theme.whiteSpace};
+    background-color: ${pr => pr.theme.primaryColor};
+    border-radius: ${pr => pr.theme.input.borderRadius};
+  }
+
+`
+const StyledErrors = styled.span `
+    color: ${pr => pr.theme.errorColor};
+    font-size: ${pr => pr.theme.fontSize.small};
+    white-space: ${pr => pr.theme.whiteSpace};
+
+`
+
+
 
 const LoginForm = (props) => {
   const { values, submit, change, disabled, errors } = props;
@@ -14,7 +58,8 @@ const LoginForm = (props) => {
   };
 
   return (
-    <form className="loginformcontainer" onSubmit={onSubmit}>
+    <StyledLoginForm className="loginformcontainer" onSubmit={onSubmit}>
+      <h2>MEMBER LOGIN</h2>
       <div>
         <p>
           <label>
@@ -26,7 +71,7 @@ const LoginForm = (props) => {
               type="text"
             />
           </label>
-          <span class="error">{errors.username}</span>
+          <StyledErrors>{errors.username}</StyledErrors>
         </p>
         <p>
           <label>
@@ -38,11 +83,11 @@ const LoginForm = (props) => {
               type="password"
             />
           </label>
-          <span class="error">{errors.password}</span>
+          <StyledErrors>{errors.password}</StyledErrors>
         </p>
       </div>
       <button disabled={disabled}>Log In</button>
-    </form>
+    </StyledLoginForm>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { store } from "./store/Store";
+import { ThemeProvider } from 'styled-components'
+import theme from './components/Theme'
 
 ReactDOM.render(
   <Provider store={store}>
+    <ThemeProvider theme = {theme}>
     <Router>
       <App />
     </Router>
+    </ThemeProvider>
   </Provider>,
   document.getElementById("root")
 );
