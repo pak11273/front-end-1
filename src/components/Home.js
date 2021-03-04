aa43b8e1a3867c0e6aad8ae7fbaf6e7f0ce7ad28
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-04 15:10:53 -0800

    homepage styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,32 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import styled from 'styled-components';
+
+const StyledHome = styled.div`
+    background-color: ${pr => pr.theme.lightestColor};
+    padding: ${pr => pr.theme.padding.backgroundPadding};
+
+    text-align: center;
+
+    img {
+        width: 50%;
+        border: 5px solid black;
+    }
+`;
 
 const Home = () => {
 
     return(
-            <div>
-                <h1>Secret Family Recipe</h1>
-                <p>Store your personal recipes on the Internet</p>
-                <div class='.links-div'>
-                    <Link to="/login">Log In</Link>
-                    <Link to="/signup">Sign Up</Link>
+            <StyledHome>
+                <div class='home'>
+                    <h1>Secret Family Recipe</h1>
+                    <p>Store your personal recipes on the Internet</p>
+                    <div class='links-div'>
+                        <Link to="/login">Log In</Link> <Link to="/signup">Sign Up</Link>
+                    </div>
+                    <div class='screenshot-div'><img src='/dashboard-screenshot.jpg'/></div>
                 </div>
-                <img src='/dashboard-screenshot.jpg'/>
-            </div>
+            </StyledHome>
     )
     }
     
