05b6b89d145a1d17d785688935f8bc78df3e452b
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-04 21:20:42 -0800

    styled New

--- src/components/New.js
+++ src/components/New.js
@@ -2,6 +2,47 @@ import React, {useState} from 'react'
 import { useHistory } from 'react-router-dom';
 import { axiosWithAuth } from '../auth/axiosWithAuth';
 import { connect } from "react-redux";
+import styled from 'styled-components';
+
+const StyledRecipeForm = styled.div`
+  color: ${pr => pr.theme.fontColor};
+  font-size: ${pr => pr.theme.fontSize.medium};
+  background-color: ${pr => pr.theme.tertiaryColor};
+  border-radius: ${pr => pr.theme.borderRadius};
+  width: 50%;
+  margin: 10px auto;
+  padding: 20px;
+
+  input {
+    font-size: 0.8em;
+    width: 40%;
+  }
+  textarea {
+    width: 100%;
+    font-size: 0.8em;
+    height: 5em;
+    resize: none;
+  }
+  input[type='submit'] {
+    color: ${pr => pr.theme.fontColor};
+    padding: ${pr => pr.theme.padding.buttonPadding};
+    white-space: ${pr => pr.theme.whiteSpace};
+    background-color: ${pr => pr.theme.primaryColor};
+    border-radius: ${pr => pr.theme.input.borderRadius};
+    font-size: 1em;
+  }
+  p {
+    text-align: center;
+  }
+  p.input-paragraph {
+    text-align: right;
+  }
+  
+`;
+
+const StyledTitle = styled.div`
+  text-align: center;
+`;
 
 function EditRecipe({user_id}) {
   
@@ -53,39 +94,41 @@ function EditRecipe({user_id}) {
 
   return (
     <div>
-      <h1>New Recipe</h1>
-      <form onSubmit={handleSubmit}>
-        <p>
-          <label>
-            Title: <input type='text' name='title' value={formState.title} onChange={handleChange}/>
-          </label>
-        </p>
-        <p>
-          <label>
-            Categories (separate with commas): <input type='text' name='category' value={formState.category} onChange={handleChange}/>
-          </label>
-        </p>
-        <p>
-          <label>
-            Source: <input type='text' name='source' value={formState.source} onChange={handleChange}/>
-          </label>
-        </p>
-        <p>
-          <label>
-            Ingredients (separate with commas): <br />
-            <textarea name='ingredients' value={formState.ingredients} onChange={handleChange}/>
-          </label>
-        </p>
-        <p>
-          <label>
-            Instructions: <br />
-            <textarea name='instructions' value={formState.instructions} onChange={handleChange}/>
-          </label>
-        </p>
-        <p>
-          <input type='submit' value='Submit'/>
-        </p>
-      </form>
+      <StyledTitle><h1>New Recipe</h1></StyledTitle>
+      <StyledRecipeForm>
+        <form onSubmit={handleSubmit}>
+          <p class='input-paragraph'>
+            <label>
+              Title: <input type='text' name='title' value={formState.title} onChange={handleChange}/>
+            </label>
+          </p>
+          <p class='input-paragraph'>
+            <label>
+              Categories (separate with commas): <input type='text' name='category' value={formState.category} onChange={handleChange}/>
+            </label>
+          </p>
+          <p class='input-paragraph'>
+            <label>
+              Source: <input type='text' name='source' value={formState.source} onChange={handleChange}/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Ingredients (separate with commas): <br />
+              <textarea name='ingredients' value={formState.ingredients} onChange={handleChange}/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Instructions: <br />
+              <textarea name='instructions' value={formState.instructions} onChange={handleChange}/>
+            </label>
+          </p>
+          <p class='button-holder'>
+            <input type='submit' value='Submit'/>
+          </p>
+        </form>
+      </StyledRecipeForm>
     </div>
   )
 }
